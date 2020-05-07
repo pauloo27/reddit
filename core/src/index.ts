b125273d9e95315b596b784a4aa52daf9a162624
Paulo <pauloeduardo2709@gmail.com>
2020-05-07 11:00:11 -0300

    ✨ Add extract info, video and audio functions

--- core/src/index.ts
+++ core/src/index.ts
@@ -1,4 +1,36 @@
-export function extractId(url: string) : string {
-  return '';
+import fetch from 'node-fetch';
+
+export interface VideoInfo {
+  fallbackUrl: string;
+  isVideo: boolean;
+  url: string;
+  duration: number;
+}
+
+export async function extractVideoInfo(url: string) : Promise<VideoInfo> {
+  let fetchUrl = url;
+  if (!fetchUrl.endsWith('.json')) fetchUrl += '.json';
+
+  const json = await (await fetch(url)).json();
+
+  const postData = json[0].data.children[0].data;
+  const mediaData = postData.media.reddit_video;
+
+  const info: VideoInfo = {
+    fallbackUrl: mediaData.fallback_url,
+    url: postData.url,
+    isVideo: postData.is_video,
+    duration: mediaData.duration,
+  };
+
+  return info;
+}
+
+export function extractAudioUrl(videoInfo : VideoInfo) : string {
+  return `${videoInfo.url}/audio`;
+}
+
+export function extractVideoUrl(videoInfo: VideoInfo) : string {
+  return videoInfo.fallbackUrl;
 }
 
