ed167e31b1e36644dff4c4c93f16bbc734e91f19
Paulo <pauloeduardo2709@gmail.com>
2020-05-07 13:29:18 -0300

    ✨ Add download command to CLI
    Close #1

--- cli/src/index.ts
+++ cli/src/index.ts
@@ -1,20 +1,65 @@
 import cli from 'commander';
-import { extractVideoInfo, extractAudioUrl, extractVideoUrl } from 'reddit-downloader-core';
+import {
+  extractVideoInfo, extractAudioUrl, extractVideoUrl, hasAudioTrack,
+} from 'reddit-downloader-core';
+import ffmpeg from 'fluent-ffmpeg';
+import fs from 'fs';
 
-cli.command("extract <url>").description("Extract the media urls from a Reddit video")
+cli.command('extract <url>').description('Extract the media urls from a Reddit video')
   .action(async (url) => {
-    console.log("Extracting " + url);
+    console.log(`Extracting ${url}`);
     const videoInfo = await extractVideoInfo(url);
-    console.log("Audio track: " + extractAudioUrl(videoInfo));
-    console.log("Video track: " + extractVideoUrl(videoInfo));
+
+    console.log(`Video track: ${extractVideoUrl(videoInfo)}`);
+
+    if (await hasAudioTrack(videoInfo)) {
+      console.log(`Audio track: ${extractAudioUrl(videoInfo)}`);
+    } else {
+      console.log('No audio found');
+    }
   });
 
-cli.on("--help", () => {
+cli.command('download <url>').description('Download a Reddit video with Audio')
+  .action(async (url) => {
+    const videoInfo = await extractVideoInfo(url);
+    const id = videoInfo.url.split('/')[3];
+
+    if (!fs.existsSync('downloads')) fs.mkdirSync('downloads');
+
+    const outputFile = `downloads/${id}.mp4`;
+    const command = ffmpeg();
+
+    command.output(outputFile).addInput(extractVideoUrl(videoInfo));
+
+    if (await hasAudioTrack(videoInfo)) {
+      console.log('Found audio track...');
+      command.addInput(extractAudioUrl(videoInfo));
+    }
+
+    command.on('error', (err) => {
+      console.error('Cannot download video: ');
+      console.error(err);
+    });
+
+    command.on('end', () => {
+      console.log(`Saved to ${outputFile}`);
+    });
+
+    command.on('progress', (data) => {
+      console.log(`Status ${data.percent.toFixed(1)}%`);
+    });
+
+    console.log('Downloading the video...');
+    command.run();
+  });
+
+cli.on('--help', () => {
   const pathToIndex = process.argv[1].substring(process.cwd().length + 1);
 
   console.log();
-  console.log("Examples:");
+  console.log('Examples:');
   console.log(`  $ node ${pathToIndex} download https://www.reddit.com/r/arduino/comments/gf4b6l/forgive_me_lord_for_i_have_sinned/`);
+  console.log(`  $ node ${pathToIndex} extract https://www.reddit.com/r/arduino/comments/gf4b6l/forgive_me_lord_for_i_have_sinned/`);
 });
 
 
